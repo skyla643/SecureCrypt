77f0edab79f57f81c7169a6ffb4ed65d803a557c
Skyla Miranda Reyes <64565983+skyla643@users.noreply.github.com>
2024-03-30 23:30:10 -0400

    Update encrypt_decrypt.py

--- encrypt_decrypt.py
+++ encrypt_decrypt.py
@@ -3,7 +3,7 @@ from cryptography.fernet import Fernet
 
 # Generate a key and save it to a file
 key = Fernet.generate_key()
-with open("C:/Users/Future5/OneDrive/Documents/GitHub/programer-rizz/encryption_key.txt", "wb") as file:
+with open("your file", "wb") as file:
     file.write(key)
 
 # Encrypt a message
@@ -12,11 +12,11 @@ fernet = Fernet(key)
 encrypted_message = fernet.encrypt(message)
 
 # Save the encrypted message to a file
-with open("C:/Users/Future5/OneDrive/Documents/GitHub/programer-rizz/encrypted_message.txt", "wb") as file:
+with open("your file", "wb") as file:
     file.write(encrypted_message)
 
 # Decrypt the message
-encrypted_message = open("C:/Users/Future5/OneDrive/Documents/GitHub/programer-rizz/encrypted_message.txt", "rb").read()
+encrypted_message = open("your file", "rb").read()
 decrypted_message = fernet.decrypt(encrypted_message)
 decrypted_message = decrypted_message.decode()
 
